b191d13298e4f5bf99906af1a88cc5ff49e50880
rocha2710 <50218071+rocha2710@users.noreply.github.com>
2022-11-13 16:49:26 -0300

    escondeTudo e mostraTudo
    criação das funções escondeTudo e mostraTudo

--- ASSETS/JS/heatmap.js
+++ ASSETS/JS/heatmap.js
@@ -1,30 +1,19 @@
-//PARA ADICIONAR A IMAGEM QUANDO CLICAR NELA USAR:
-//$( "<b>hello</b>" ).appendTo( "body" ); 
-
 $(document).ready(function () {
     console.log("start system ... ");
+    //INCLUSÃO DE BOTÕES PARA DICIONAR OS ICONES
     $( "<img src='/ICONES/capacete.png' width='100' height='100' class='capaceteAdd'/>" ).appendTo( "#addIcones");
-    console.log("start system ... ");
     $( "<img src='/ICONES/NCAmbientalMenor.png' width='100' height='100' class='NCAMenAdd'/>" ).appendTo( "#addIcones");
-    console.log("start system ... ");
-    
     $( "<img src='/ICONES/NCAmbientalMaior.png' width='100' height='100' class='NCAMaiAdd'/>" ).appendTo( "#addIcones");
-    
     $( "<img src='/ICONES/eventoAp.png' width='100' height='100' class='EventoApAdd'/>" ).appendTo( "#addIcones");
-    
     $( "<img src='/ICONES/eventograve.png' width='100' height='100' class='EventoGraveAdd'/>" ).appendTo( "#addIcones");
-    
     $( "<img src='/ICONES/eventongrave.png' width='100' height='100' class='EventoNGraveAdd'/>" ).appendTo( "#addIcones");
-    
-    $( ".capaceteAdd" ).on("click", {ic:"capacete"}, addIcon);
-    console.log("start system ... ");    
-    $( ".NCAMenAdd" ).on("click", {ic:"NCAmbientalMenor"}, addIcon);
-    console.log("start system ... ");    
+    //ATRELADO A FUNÇÃO addIcon AOS BOTÕES
+    $( ".capaceteAdd" ).on("click", {ic:"capacete"}, addIcon);       
+    $( ".NCAMenAdd" ).on("click", {ic:"NCAmbientalMenor"}, addIcon);  
     $( ".NCAMaiAdd" ).on("click", {ic:"NCAmbientalMaior"}, addIcon);    
     $( ".EventoApAdd" ).on("click", {ic:"eventoAp"}, addIcon);    
     $( ".EventoGraveAdd" ).on("click", {ic:"eventograve"}, addIcon);    
     $( ".EventoNGraveAdd" ).on("click", {ic:"eventongrave"}, addIcon);
-    
     
     function addIcon (event) {
         var data = event.data;
@@ -32,39 +21,103 @@ $(document).ready(function () {
             //capacete
             case "capacete":
                 $( "<img src='/ICONES/capacete.png' width='100' height='100' class='capaceteIco' width='100' height='100' class='NCAMenIco'/>" ).appendTo( "#addIcones");
-                $( ".capaceteIco" ).draggable();  
+                $( ".capaceteIco" ).draggable().on("click", {ref:"capacete"}, escondeTudo).on("dblclick", mostraTudo);
                 break;
             //NCAmbientalMenor
             case "NCAmbientalMenor":
                 $( "<img src='/ICONES/NCAmbientalMenor.png' width='100' height='100' class='NCAMenIco'/>" ).appendTo( "#addIcones");
-                $( ".NCAMenIco" ).draggable();  
+                $( ".NCAMenIco" ).draggable().on("click", {ref:"NCAmbientalMenor"}, escondeTudo).on("dblclick", mostraTudo); 
                 break;
             //NCAmbientalMaior
             case "NCAmbientalMaior":
                 $( "<img src='/ICONES/NCAmbientalMaior.png' width='100' height='100' class='NCAMaiIco'/>" ).appendTo( "#addIcones");
-                $( ".NCAMaiIco" ).draggable();    
+                $( ".NCAMaiIco" ).draggable().on("click", {ref:"NCAmbientalMaior"}, escondeTudo).on("dblclick", mostraTudo);  
                 break;
             //eventoAp
             case "eventoAp":
                 $( "<img src='/ICONES/eventoAp.png' width='100' height='100' class='eventoApIco'/>" ).appendTo( "#addIcones");
-                $( ".eventoApIco" ).draggable();    
+                $( ".eventoApIco" ).draggable().on("click", {ref:"eventoAp"}, escondeTudo).on("dblclick", mostraTudo);  
                 break;
             //eventograve
             case "eventograve":
                 $( "<img src='/ICONES/eventograve.png' width='100' height='100' class='eventograveIco'/>" ).appendTo( "#addIcones");
-                $( ".eventograveIco" ).draggable();    
+                $( ".eventograveIco" ).draggable().on("click", {ref:"eventograve"}, escondeTudo).on("dblclick", mostraTudo);    
                 break;
             //eventongrave
             case "eventongrave":
                 $( "<img src='/ICONES/eventongrave.png' width='100' height='100' class='eventongraveIco'/>" ).appendTo( "#addIcones");
-                $( ".eventongraveIco" ).draggable();    
+                $( ".eventongraveIco" ).draggable().on("click", {ref:"eventongrave"}, escondeTudo).on("dblclick", mostraTudo);    
                 break;
-        
+            //default não faz nada apenas para.
+                default:
+                break;
+        }   
+    } //function addIcon
+    
+    function escondeTudo (event) {
+        var data = event.data;
+        switch (data.ref) {
+            //capacete
+            case "capacete":
+                //$( ".capaceteIco" ).addClass("esconde");
+                $( ".NCAMenIco" ).addClass("esconde");
+                $( ".NCAMaiIco" ).addClass("esconde");
+                $( ".eventoApIco" ).addClass("esconde");
+                $( ".eventograveIco" ).addClass("esconde");
+                $( ".eventongraveIco" ).addClass("esconde");
+                break;
+            //NCAmbientalMenor
+            case "NCAmbientalMenor":
+                $( ".capaceteIco" ).addClass("esconde");
+                //$( ".NCAMenIco" ).addClass("esconde");
+                $( ".NCAMaiIco" ).addClass("esconde");
+                $( ".eventoApIco" ).addClass("esconde");
+                $( ".eventograveIco" ).addClass("esconde");
+                $( ".eventongraveIco" ).addClass("esconde");
+                break;
+            //NCAmbientalMaior
+            case "NCAmbientalMaior":
+                $( ".capaceteIco" ).addClass("esconde");
+                $( ".NCAMenIco" ).addClass("esconde");
+                //$( ".NCAMaiIco" ).addClass("esconde");
+                $( ".eventoApIco" ).addClass("esconde");
+                $( ".eventograveIco" ).addClass("esconde");
+                $( ".eventongraveIco" ).addClass("esconde");    
+                break;
+            //eventoAp
+            case "eventoAp":
+                $( ".capaceteIco" ).addClass("esconde");
+                $( ".NCAMenIco" ).addClass("esconde");
+                $( ".NCAMaiIco" ).addClass("esconde");
+                //$( ".eventoApIco" ).addClass("esconde");
+                $( ".eventograveIco" ).addClass("esconde");
+                $( ".eventongraveIco" ).addClass("esconde");    
+                break;
+            //eventograve
+            case "eventograve":
+                $( ".capaceteIco" ).addClass("esconde");
+                $( ".NCAMenIco" ).addClass("esconde");
+                $( ".NCAMaiIco" ).addClass("esconde");
+                $( ".eventoApIco" ).addClass("esconde");
+                //$( ".eventograveIco" ).addClass("esconde");
+                $( ".eventongraveIco" ).addClass("esconde");    
+                break;
+            //eventongrave
+            case "eventongrave":
+                $( ".capaceteIco" ).addClass("esconde");
+                $( ".NCAMenIco" ).addClass("esconde");
+                $( ".NCAMaiIco" ).addClass("esconde");
+                $( ".eventoApIco" ).addClass("esconde");
+                $( ".eventograveIco" ).addClass("esconde");
+                //$( ".eventongraveIco" ).addClass("esconde");   
+                break;
+            //default não faz nada apenas para.
                 default:
                 break;
         }
-        
-    }
+    } //function escondeTudo
     
-    
+    function mostraTudo () {
+       $("*").removeClass("esconde");
+    } //function mostraTudo
 });
